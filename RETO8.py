ed226e4ff3bcc4f07fefc4569b101c10cdf75251
NachoOFC <ignacioperezolavarria@gmail.com>
2025-08-07 22:06:19 -0400

    reto 8 pendiente

---
+++ RETO8.py
@@ -0,0 +1,19 @@
+#8 CONTANDO PALABRAS
+"""""
+ Crea un programa que cuente cuantas veces se repite cada palabra
+ * y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que
+ *   lo resuelvan automáticamente.
+"""
+
+def contador_palabras(frase):
+    #frase en miniscula
+    frase = frase.lower()
+
+    
+    contador = 0
+    return print(contador)
+
+contador_palabras("hola hola")
